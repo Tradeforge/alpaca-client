b902d2ef2456133802b0d91249adc69b12909845
Marek Cermak <marek.cermak@tradeforge.app>
2024-10-22 21:22:18 +0200

    fix: add missing pointer to instant funding request data
    Signed-off-by: Marek Cermak <marek.cermak@tradeforge.app>

--- broker/funding.go
+++ broker/funding.go
@@ -38,7 +38,7 @@ func (fc *FundingClient) GetFundingDetails(ctx context.Context, params model.Get
 	return res, err
 }
 
-func (fc *FundingClient) CreateInstantFundingRequest(ctx context.Context, data model.CreateInstantFundingRequest, opts ...model.RequestOption) (*model.CreateInstantFundingResponse, error) {
+func (fc *FundingClient) CreateInstantFundingRequest(ctx context.Context, data *model.CreateInstantFundingRequest, opts ...model.RequestOption) (*model.CreateInstantFundingResponse, error) {
 	res := &model.CreateInstantFundingResponse{}
 	err := fc.Call(ctx, http.MethodPost, CreateInstantDepositPath, nil, res, append(opts, model.Body(data))...)
 	return res, err
