c16a6a287470de070f1bf089b114313d87716cf1
Marek Cermak <marek.cermak@tradeforge.app>
2024-11-11 15:35:55 +0100

    fix: fix typo in market clock response
    Signed-off-by: Marek Cermak <marek.cermak@tradeforge.app>

--- model/clock.go
+++ model/clock.go
@@ -4,7 +4,7 @@ import "time"
 
 type GetMarketClockResponse struct {
 	Timestamp time.Time `json:"timestamp"`
-	IsOpen    bool      `json:"is_ope"`
+	IsOpen    bool      `json:"is_open"`
 	NextOpen  time.Time `json:"next_open"`
 	NextClose time.Time `json:"next_close"`
 }
