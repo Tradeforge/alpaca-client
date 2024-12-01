79ccb97f3d617ddcaf7e2e2ab616533cba720c27
Marek Cermak <marek.cermak@tradeforge.app>
2024-12-01 17:42:10 +0100

    chore: reformat
    Signed-off-by: Marek Cermak <marek.cermak@tradeforge.app>

--- broker/event.go
+++ broker/event.go
@@ -1,24 +1,24 @@
 package broker
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
+	"context"
+	"encoding/json"
+	"fmt"
 
-    "go.tradeforge.dev/alpaca/client"
-    "go.tradeforge.dev/alpaca/model"
-    "go.tradeforge.dev/alpaca/sse"
+	"go.tradeforge.dev/alpaca/client"
+	"go.tradeforge.dev/alpaca/model"
+	"go.tradeforge.dev/alpaca/sse"
 )
 
 const (
-    GetOrderEventsPath         = "/v2beta1/events/trades"
-    GetTransferEventPath       = "/v1/events/transfers/status"
-    GetAccountStatusEventsPath = "/v1/events/accounts/status"
+	GetOrderEventsPath         = "/v2beta1/events/trades"
+	GetTransferEventPath       = "/v1/events/transfers/status"
+	GetAccountStatusEventsPath = "/v1/events/accounts/status"
 )
 
 // EventClient defines a client for the Alpaca Broker Event API.
 type EventClient struct {
-    *client.Client
+	*client.Client
 }
 
 type AccountStatusUpdateHandler func(ctx context.Context, event *model.AccountStatusUpdateEvent) error
@@ -27,47 +27,47 @@ type AccountStatusUpdateHandler func(ctx context.Context, event *model.AccountSt
 // The handler will be called for each event received.
 // This is a blocking call.
 func (c *EventClient) ListenToAccountStatusUpdateEvents(ctx context.Context, params model.WatchParams, handler AccountStatusUpdateHandler, opts ...model.RequestOption) error {
-    return c.Listen(
-        ctx,
-        GetAccountStatusEventsPath,
-        params,
-        wrapAccountStatusUpdateHandler(handler),
-        opts...,
-    )
+	return c.Listen(
+		ctx,
+		GetAccountStatusEventsPath,
+		params,
+		wrapAccountStatusUpdateHandler(handler),
+		opts...,
+	)
 }
 
 // SubscribeToAccountStatusUpdateEvents subscribes to account status update SSE events.
 // The handler will be called for each event received.
 // This is a non-blocking call.
 func (c *EventClient) SubscribeToAccountStatusUpdateEvents(ctx context.Context, params model.WatchParams, handler AccountStatusUpdateHandler, opts ...model.RequestOption) error {
-    return c.Subscribe(
-        ctx,
-        GetAccountStatusEventsPath,
-        params,
-        wrapAccountStatusUpdateHandler(handler),
-        opts...,
-    )
+	return c.Subscribe(
+		ctx,
+		GetAccountStatusEventsPath,
+		params,
+		wrapAccountStatusUpdateHandler(handler),
+		opts...,
+	)
 }
 
 func wrapAccountStatusUpdateHandler(handler AccountStatusUpdateHandler) client.EventStreamHandler {
-    return func(ctx context.Context, event *sse.Event) error {
-        if event.IsComment() {
-            return nil
-        }
-        e, err := parseAccountStatusUpdateEvent(event)
-        if err != nil {
-            return fmt.Errorf("parsing account status event: %w", err)
-        }
-        return handler(ctx, e)
-    }
+	return func(ctx context.Context, event *sse.Event) error {
+		if event.IsComment() {
+			return nil
+		}
+		e, err := parseAccountStatusUpdateEvent(event)
+		if err != nil {
+			return fmt.Errorf("parsing account status event: %w", err)
+		}
+		return handler(ctx, e)
+	}
 }
 
 func parseAccountStatusUpdateEvent(event *sse.Event) (*model.AccountStatusUpdateEvent, error) {
-    e := model.AccountStatusUpdateEvent{}
-    if err := json.Unmarshal(event.Data, &e); err != nil {
-        return nil, fmt.Errorf("unmarshalling account status event: %w", err)
-    }
-    return &e, nil
+	e := model.AccountStatusUpdateEvent{}
+	if err := json.Unmarshal(event.Data, &e); err != nil {
+		return nil, fmt.Errorf("unmarshalling account status event: %w", err)
+	}
+	return &e, nil
 }
 
 type TransferStatusUpdateEventHandler func(ctx context.Context, event *model.TransferStatusUpdateEvent) error
@@ -76,47 +76,47 @@ type TransferStatusUpdateEventHandler func(ctx context.Context, event *model.Tra
 // The handler will be called for each event received.
 // This is a blocking call.
 func (c *EventClient) ListenToTransferEvents(ctx context.Context, params model.WatchParams, handler TransferStatusUpdateEventHandler, opts ...model.RequestOption) error {
-    return c.Listen(
-        ctx,
-        GetTransferEventPath,
-        params,
-        wrapTransferEventHandler(handler),
-        opts...,
-    )
+	return c.Listen(
+		ctx,
+		GetTransferEventPath,
+		params,
+		wrapTransferEventHandler(handler),
+		opts...,
+	)
 }
 
 // SubscribeToTransferEvents subscribes to transfer status update SSE events.
 // The handler will be called for each event received.
 // This is a non-blocking call.
 func (c *EventClient) SubscribeToTransferEvents(ctx context.Context, params model.WatchParams, handler TransferStatusUpdateEventHandler, opts ...model.RequestOption) error {
-    return c.Subscribe(
-        ctx,
-        GetTransferEventPath,
-        params,
-        wrapTransferEventHandler(handler),
-        opts...,
-    )
+	return c.Subscribe(
+		ctx,
+		GetTransferEventPath,
+		params,
+		wrapTransferEventHandler(handler),
+		opts...,
+	)
 }
 
 func wrapTransferEventHandler(handler TransferStatusUpdateEventHandler) client.EventStreamHandler {
-    return func(ctx context.Context, event *sse.Event) error {
-        if event.IsComment() {
-            return nil
-        }
-        e, err := parseTransferEvent(event)
-        if err != nil {
-            return fmt.Errorf("parsing transfer event: %w", err)
-        }
-        return handler(ctx, e)
-    }
+	return func(ctx context.Context, event *sse.Event) error {
+		if event.IsComment() {
+			return nil
+		}
+		e, err := parseTransferEvent(event)
+		if err != nil {
+			return fmt.Errorf("parsing transfer event: %w", err)
+		}
+		return handler(ctx, e)
+	}
 }
 
 func parseTransferEvent(event *sse.Event) (*model.TransferStatusUpdateEvent, error) {
-    e := model.TransferStatusUpdateEvent{}
-    if err := json.Unmarshal(event.Data, &e); err != nil {
-        return nil, fmt.Errorf("unmarshalling transfer event: %w", err)
-    }
-    return &e, nil
+	e := model.TransferStatusUpdateEvent{}
+	if err := json.Unmarshal(event.Data, &e); err != nil {
+		return nil, fmt.Errorf("unmarshalling transfer event: %w", err)
+	}
+	return &e, nil
 }
 
 type OrderEventHandler func(ctx context.Context, event *model.OrderEvent) error
@@ -125,45 +125,45 @@ type OrderEventHandler func(ctx context.Context, event *model.OrderEvent) error
 // The handler will be called for each event received.
 // This is a blocking call.
 func (c *EventClient) ListenToOrderEvents(ctx context.Context, params model.WatchParams, handler OrderEventHandler, opts ...model.RequestOption) error {
-    return c.Listen(
-        ctx,
-        GetOrderEventsPath,
-        params,
-        wrapOrderEventHandler(handler),
-        opts...,
-    )
+	return c.Listen(
+		ctx,
+		GetOrderEventsPath,
+		params,
+		wrapOrderEventHandler(handler),
+		opts...,
+	)
 }
 
 // SubscribeToOrderEvents subscribes to order SSE events.
 // The handler will be called for each event received.
 // This is a non-blocking call.
 func (c *EventClient) SubscribeToOrderEvents(ctx context.Context, params model.WatchParams, handler OrderEventHandler, opts ...model.RequestOption) error {
-    return c.Listen(
-        ctx,
-        GetOrderEventsPath,
-        params,
-        wrapOrderEventHandler(handler),
-        opts...,
-    )
+	return c.Listen(
+		ctx,
+		GetOrderEventsPath,
+		params,
+		wrapOrderEventHandler(handler),
+		opts...,
+	)
 }
 
 func wrapOrderEventHandler(handler OrderEventHandler) client.EventStreamHandler {
-    return func(ctx context.Context, event *sse.Event) error {
-        if event.IsComment() {
-            return nil
-        }
-        e, err := parseOrderEvent(event)
-        if err != nil {
-            return fmt.Errorf("parsing order event: %w", err)
-        }
-        return handler(ctx, e)
-    }
+	return func(ctx context.Context, event *sse.Event) error {
+		if event.IsComment() {
+			return nil
+		}
+		e, err := parseOrderEvent(event)
+		if err != nil {
+			return fmt.Errorf("parsing order event: %w", err)
+		}
+		return handler(ctx, e)
+	}
 }
 
 func parseOrderEvent(event *sse.Event) (*model.OrderEvent, error) {
-    e := model.OrderEvent{}
-    if err := json.Unmarshal(event.Data, &e); err != nil {
-        return nil, fmt.Errorf("unmarshalling order event: %w", err)
-    }
-    return &e, nil
+	e := model.OrderEvent{}
+	if err := json.Unmarshal(event.Data, &e); err != nil {
+		return nil, fmt.Errorf("unmarshalling order event: %w", err)
+	}
+	return &e, nil
 }
